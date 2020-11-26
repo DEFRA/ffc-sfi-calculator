2d2ceadf825bad22c8d81f16778662b7e73722a9
Paul Andrews <paul.andrews@defra.gov.uk>
2020-11-26 12:21:27 +0000

    Remove testing code and redundant rename script

--- app/index.js
+++ app/index.js
@@ -3,12 +3,6 @@ const server = require('./server')
 const init = async () => {
   await server.start()
   console.log('Server running on %s', server.info.uri)
-
-  const messageAction = function (message) {
-    console.log(message.body)
-  }
-
-  require('./messaging/receivers').startAgreementChanged(messageAction)
 }
 
 process.on('unhandledRejection', (err) => {
