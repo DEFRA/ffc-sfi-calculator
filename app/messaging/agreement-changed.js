66a122616b2689f3cba2297fe2a8c4486a930d7b
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-17 13:36:49 +0000

    feat: run calc on all msg props

--- app/messaging/agreement-changed.js
+++ app/messaging/agreement-changed.js
@@ -1,7 +1,18 @@
+function calcPayment (k, v) {
+  const totalArea = Number(v.totalArea)
+  // set default value of 100 if area isn't a number
+  return Number.isNaN(totalArea) ? 100 : totalArea * v.paymentRate
+}
+
 module.exports = async function (msg) {
   const { body, correlationId } = msg
-  const result = Number(body.value)
-  // set default value of 123
-  body.result = Number.isNaN(result) ? 123 : result * 10
+  let totalPayment = 0
+
+  Object.entries(body).forEach(([k, v]) => {
+    const payment = calcPayment(k, v)
+    body[k].payment = payment
+    totalPayment += payment
+  })
+  body.totalPayment = totalPayment
   await require('./senders').updateAgreement({ body, correlationId })
 }
